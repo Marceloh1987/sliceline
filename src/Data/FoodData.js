08801240c0793fce037080e08ed78910b554de1f
marceloh1987 <marceloh1987@gmail.com>
2019-12-17 03:49:30 -0300

    Add Menu, Food Data and FoodGrid

---
+++ src/Data/FoodData.js
@@ -0,0 +1,34 @@
+export const foods = [
+    {
+        name: 'Cheese Pizza',
+        img: '/img/pizza1.jpg'
+    },
+    {
+        name: 'Pepperoni Pizza',
+        img: '/img/pizza2.jpg'
+    },
+    {
+        name: 'Shrimp Pizza',
+        img: '/img/pizza3.jpg'
+    },
+    {
+        name: 'CHicken Pizza',
+        img: '/img/pizza4.jpg'
+    },
+    {
+        name: 'Veggie Pizza',
+        img: '/img/pizza5.jpg'
+    },
+    {
+        name: 'Burger Pizza',
+        img: '/img/pizza6.jpg'
+    },
+    {
+        name: 'Gyro Pizza',
+        img: '/img/pizza7.jpg'
+    },
+    {
+        name: 'Frie Pizza',
+        img: '/img/pizza8.jpg'
+    }
+];
