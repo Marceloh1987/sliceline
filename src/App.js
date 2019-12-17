862207b157e23caefab712790fb253abc4686836
marceloh1987 <marceloh1987@gmail.com>
2019-12-17 03:45:25 -0300

    Add Menu, Food Data and FoodGrid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components'
 import { Navbar  } from './Navbar/Navbar';
 import { Banner } from './Banner/Banner';
+import { Menu } from './Menu/Menu';
 
 const Globalstyle = createGlobalStyle`
   body {
@@ -19,6 +20,7 @@ function App() {
     <Globalstyle/>
     <Navbar/>
     <Banner/>
+    <Menu/>
     </>
   );
 }
